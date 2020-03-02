9ff8b6b5f417da90c4ddb096d487d6d49dac23de
Todd Spainhour <toddspainhour@gmail.com>
2020-03-01 21:20:53 -0600

    styling complete

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -9,11 +9,11 @@ const makeRiverCards = (arr) => {
     let domString = '';
     for (let i = 0; i < arr.length; i++) {
     
-
+    domString +=    `<div class="col-md-3 bg-light mt-5 mr-3 ml-3"`
     domString +=   `<div class="card">`
-    domString +=            `<img src="${arr[i].image}" class="card-img-top" alt="...">`
-    domString +=        `<div class="card-body col-6">`
-    domString +=            `<p class="card-text">${arr[i].name}</p>`
+    domString +=            `<img src="${arr[i].image}" class="card-img-top mt-2 rounded" alt="...">`
+    domString +=        `<div class="card-body">`
+    domString +=            `<h3 class="card-text text-center">${arr[i].name}</h3>`
     domString +=        `</div>`
     domString +=   `</div>`
 
